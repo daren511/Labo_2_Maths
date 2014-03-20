18de69cb7e0c534ebded765b1067a46b3ccab576
Daren-Ken St-laurent <daren511@hotmail.com>
2014-03-20 13:47:02 -0400

    wif comments

--- Labo2/Form1.cs
+++ Labo2/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿///////////////////////////////////////////////////////////////////////////
+/////////////           Labo 02 Math Pour Jonathan Lortie       ///////////
+/////////////               Francis Cote                        ///////////                       
+/////////////               Mathieu Dumoulin                    ///////////
+/////////////               Daren-Ken St-laurent                ///////////
+///////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -60,13 +67,13 @@ namespace Labo2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ReadCoordinates(FileName);
-            CMB_Prob.Items.Add(">");
+            ReadCoordinates(FileName); // lecture des donnes du tableau
+            CMB_Prob.Items.Add(">"); // ajout des objets dans le combobox(choix de l'usager)
             CMB_Prob.Items.Add("<");
             CMB_Prob.Items.Add("< >");
         }
 
-        private void BTN_Calculer_Click(object sender, EventArgs e)
+        private void BTN_Calculer_Click(object sender, EventArgs e) // methode appeler au moment du clique sur le bouton calculer
         {
             bool choix2Bornes = false;
 
@@ -84,10 +91,10 @@ namespace Labo2
                 try
                 {
                     // Premiere borne
-                    Borne1 = Convert.ToDouble(TB_Borne1.Text);
-                    Moyenne = Convert.ToDouble(TB_Moyenne.Text);
-                    double EcartType = Convert.ToDouble(TB_EcartType.Text);
-                    double CoteZ1 = (Borne1 - Moyenne) / EcartType;
+                    Borne1 = Convert.ToDouble(TB_Borne1.Text);//convertition valeur du texte en double
+                    Moyenne = Convert.ToDouble(TB_Moyenne.Text);//convertition valeur du texte en double
+                    double EcartType = Convert.ToDouble(TB_EcartType.Text); //converti la valeur pour avoir des decimal
+                    double CoteZ1 = (Borne1 - Moyenne) / EcartType; //calcul de la cote Z
                     float Prob1 = 0.0f;
                     // Deuxieme borne
                     double CoteZ2 = 0;
@@ -95,14 +102,14 @@ namespace Labo2
 
                     if (TB_Borne2.Text != "")
                     {
-                        Borne2 = Convert.ToDouble(TB_Borne2.Text);
-                        CoteZ2 = (Borne2 - Moyenne) / EcartType;
+                        Borne2 = Convert.ToDouble(TB_Borne2.Text); //converti  la valeur du texte en double
+                        CoteZ2 = (Borne2 - Moyenne) / EcartType; // effectue la cote Z
                     }
 
 
                     if (CoteZ1 >= 4 || CoteZ1 <= -4) // Maximum du tableau de la cote Z
                     {
-                        MessageBox.Show("Erreur! La cote Z n'est pas calculable avec le tableau donné.");
+                        MessageBox.Show("Erreur! La cote Z n'est pas calculable avec le tableau donné."); //affiche 
                     }
                     else  // Si c'est une valeur 
                     {
@@ -111,10 +118,10 @@ namespace Labo2
                         if (Borne2 != 0)
                         {
                             isolerPartieNombre(CoteZ2, true);
-                            Prob2 = trouverDansTableau(Centieme2, nbArrondisDixieme2);
+                            Prob2 = trouverDansTableau(Centieme2, nbArrondisDixieme2); // affecte le resultat dans le tableau
                         }
                         Prob1 = trouverDansTableau(Centieme, nbArrondisDixieme);
-                        CalculerProbabilite(Prob1, Prob2);
+                        CalculerProbabilite(Prob1, Prob2); // calcul les probabilites
                     }
 
                 }
@@ -138,18 +145,18 @@ namespace Labo2
             {
                 case 0: // > PROOF
                     {
-                        if (!deuxBorne)
+                        if (!deuxBorne) // si pas 2 borne
                         {
-                            if (Borne1 > Moyenne)
+                            if (Borne1 > Moyenne) // Si 
                                 probFinale = 1 - prob1;
                             else
                                 probFinale = prob1;
                         }
-                        else
+                        else // si 2 bornes
                         {
-                            if (Borne1 < Borne2)
+                            if (Borne1 < Borne2)//on remplace la prob1 par la prob 2 car elle est plus grande
                                 prob1 = prob2;
-                            if (Borne1 > Moyenne)
+                            if (Borne1 > Moyenne) // 
                                 probFinale = 1 - prob1;
                             else
                                 probFinale = prob1;
@@ -177,7 +184,7 @@ namespace Labo2
                         }
                         break;
                     }
-                case 2: //< >
+                case 2: //< > PROOF
                     {
                         Gprob1 = prob1;
                         Gprob2 = prob2;
@@ -201,8 +208,8 @@ namespace Labo2
                         break;
                     }
             }
-            probFinale = (float)Math.Round(100 * probFinale);
-            TB_Resultat.Text = probFinale.ToString() + "%";
+            probFinale = (float)Math.Round(100 * probFinale); // arrondi la probabilite finale
+            TB_Resultat.Text = probFinale.ToString() + "%"; // affecte le resultat dans la text box
         }
         private void isolerPartieNombre(double CoteZ, bool deuxiemeProb) // Si cest la deuxieme prob ou non
         {
@@ -220,13 +227,13 @@ namespace Labo2
         }
         private void mettrePositif()
         {
-            if (Borne1 < Moyenne)
+            if (Borne1 < Moyenne) // verifier si la premiere borne 1 est plus grande que la moyenne
             {
-                Gprob1 = 1 - Gprob1;
+                Gprob1 = 1 - Gprob1; //on affecter l'inverse ( 100% - prob1 )
             }
-            if (Borne2 < Moyenne)
+            if (Borne2 < Moyenne)// verifier si la premiere borne 2 est plus grande que la moyenne
             {
-                Gprob2 = 1 - Gprob2;
+                Gprob2 = 1 - Gprob2; //on affecte l'inverse ( 100% - prob2 )
             }
 
         }
@@ -250,7 +257,7 @@ namespace Labo2
                     posY = i;
                 }
             }
-            return tab[posY][posX];
+            return tab[posY][posX]; // retourne un tablea
         }
     }
 }
